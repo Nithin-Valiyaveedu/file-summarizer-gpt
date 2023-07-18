d52cff8a4d788178449b809436e261a57c1f2cd1
nithin-accubits <nithin.v@accubits.com>
2023-07-18 12:46:37 +0530

    added additional text for loader

--- app/dashboard/chat-prompt/[projectId]/page.tsx
+++ app/dashboard/chat-prompt/[projectId]/page.tsx
@@ -29,7 +29,7 @@ const PromptPage = () => {
 
   return (
     <>
-      {commonLoader && <Loader />}
+      {commonLoader && <Loader text="Intiatiation..." />}
       <ChatPrompt projectId={projectId} />
     </>
   );
--- components/loader/index.js
+++ components/loader/index.js
@@ -1,10 +1,11 @@
 
 import ClipLoader from "react-spinners/ClipLoader";
 
-const Loader = () => {
+const Loader = ({ text }) => {
   return (<>
     <div className={`fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 opacity-70 inset-0 bg-black flex-col flex-center`}>
       <ClipLoader color="#BE7627" />
+      <p className="font-bold text-white">{text}</p>
     </div>
     <div className="opacity-30 fixed inset-0 z-40 bg-black" />
 
