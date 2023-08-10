eedff60ff05b0d35ba723c58b186d3cdde87f629
nithin-accubits <nithin.v@accubits.com>
2023-08-10 12:33:16 +0530

    added plugin for linebreak

--- components/chatprompt/aiPrompt.tsx
+++ components/chatprompt/aiPrompt.tsx
@@ -1,17 +1,18 @@
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import Image from "next/image";
 
+import remarkBreaks from "remark-breaks";
+
 const AiPrompt = ({ content }: { content: any }) => {
+  const source = content.replace(/\n/gi, "\n &nbsp;");
   return (
     <>
       <div className="relative flex w-[70%] mx-auto mt-4">
         <ReactMarkdown
-          className="bg-shadow p-2 bg-answerPrompt rounded-lg words w-full"
-          remarkPlugins={[remarkGfm]}>
-          {content}
-        </ReactMarkdown>
-
+          className="bg-shadow p-2 bg-answerPrompt rounded-lg words w-full "
+          remarkPlugins={[remarkBreaks]}
+          children={source}
+        />
         <div className="absolute -right-12 flex-center p-2 w-10 h-10 bg-white rounded-full">
           <Image
             src="/assets/logos/SiteLogo.svg"
